979390a6ff0dabe205656dd35c40e1677ebddacf
Stas-corp <zakyska1301@gmail.com>
2025-02-12 10:56:45 +0200

    Clearing comments and trash

--- Task1/scraper.py
+++ Task1/scraper.py
@@ -16,7 +16,6 @@ MAX_WORKERS_DB = 1
 def db_worker() -> None:
     while True:
         product = db_queue.get()
-        # print(f'db_worker {product}')
         if product is None:
             db_queue.task_done()
             break
@@ -26,7 +25,6 @@ def db_worker() -> None:
 def scrape_product_worker():
     while True:
         item = scrap_queue.get()
-        # print(f'scrape_product_worker {item}')
         if item is None:
             scrap_queue.task_done()
             break
@@ -54,7 +52,6 @@ def scrape_product(url: str, category: str) -> None:
             price = price.split('$') # ['Median:\xa0', '36,000', '12,500', '165,000LowHigh']
             price_range = f'{price[2]}; {price[1]}; {price[3][:-7]}'
         except Exception as e:
-            # print(f'Exseption search price: {url}', e)
             price_range = ''
         description = soup.find('div', class_='rt-Flex rt-r-fd-column rt-r-gap-2').text
     
@@ -76,9 +73,7 @@ def scrape_subcategorys(url: str) -> None:
             soup, response = check_response(full_url)
             if response.status_code == 200:
                 section = soup.find_all('section')
-                # print(section[1])
                 product_card = section[1].find_all('a')
-                # print(f"\n ".join(link.get('href') for link in product_card))
                 for p_url in product_card:
                     product_url = BASE_URL + p_url.get('href')
                     scrap_queue.put((product_url, category))
@@ -105,22 +100,15 @@ def start_scraping() -> None:
     
     for category in CATEGORIES:
         category_url = f"{BASE_URL}/categories/{category}"
-        # response = requests.get(category_url, headers={"User-Agent": "Mozilla/5.0"})
         soup, response = check_response(category_url)
         if response.status_code == 200:
-            # with open(f'data/{category}.html', 'w', encoding='utf-8') as file:
-            #     file.write(response.text)
-            # soup = BeautifulSoup(response.text, "lxml")
             all_subcategory = soup.find('h2', class_='rt-Heading rt-r-size-4', string='Browse all categories').parent
             subcategory_links = all_subcategory.find_all("a", href=lambda x: x and f"/categories/{category}" in x)
             for link in subcategory_links[:1]:
                 print(link.get('href'))
                 thread = threading.Thread(target=scrape_subcategorys, args=(link.get('href'),), daemon=True)
-                # threads.append(thread)
                 thread.start()
-                # thread.join()
 
-    print(f'\n\n___________\n ENDING THREAD {threads}, {scrap_queue}\n___________\n\n')
     scrap_queue.join()
     
     for _ in range(MAX_WORKERS_SCRAP):
@@ -129,7 +117,6 @@ def start_scraping() -> None:
     for thread in threads:
         thread.join()
         
-    print(f'\n\n___________\nEND THREAD {threads}, {scrap_queue}\n___________\n\n')
     db_queue.join()
     db_queue.put(None)
     db_thr.join()
@@ -138,4 +125,3 @@ if __name__ == '__main__':
     import db
     db.init_db()
     start_scraping()
-    
